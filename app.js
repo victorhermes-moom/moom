53d56dd34645852c7425fccce730e759e9d9c945
Victor Hermes <me.victorhermes@gmail.com>
2018-11-03 17:56:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var userRouter = require('./routes/user');
 var app = express();
 
 mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://localhost:27017/shopping', {
+  mongoose.connect('mongodb://node-shop:mHD7194qwerty@@node-rest-api-shard-00-00-hcj0k.mongodb.net:27017,node-rest-api-shard-00-01-hcj0k.mongodb.net:27017,node-rest-api-shard-00-02-hcj0k.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-api-shard-0&authSource=admin&retryWrites=true', {
     useMongoClient: true,
     /* other options */ 
   }
